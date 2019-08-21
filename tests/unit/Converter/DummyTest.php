9f7cc15edf4ab83fdfe28906caf8a27dc535c8f1
guvra <guvra@smile.fr>
2019-08-21 17:49:23 +0200

    Add a unit test assertion to test the Dummy converter

---
+++ tests/unit/Converter/DummyTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Smile\GdprDump\Tests\Unit\Converter;
+
+use Smile\GdprDump\Converter\Dummy;
+use Smile\GdprDump\Tests\Unit\TestCase;
+
+class DummyTest extends TestCase
+{
+    /**
+     * Test the converter.
+     */
+    public function testConverter()
+    {
+        $converter = new Dummy();
+        $value = $converter->convert('notAnonymized');
+        $this->assertSame('notAnonymized', $value);
+    }
+}
