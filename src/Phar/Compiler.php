0008ed48ac21b14dc7dc4f3d8c99160e5bebfbfc
guvra <guvra@smile.fr>
2022-02-14 12:59:25 +0100

    Fix PHP annotations being removed from the phar file

--- src/Phar/Compiler.php
+++ src/Phar/Compiler.php
@@ -69,14 +69,50 @@ class Compiler
         foreach ($this->getFinders() as $finder) {
             foreach ($finder as $file) {
                 $path = $this->getRelativeFilePath($file);
-                $phar->addFromString($path, php_strip_whitespace($file->getRealPath()));
+                $phar->addFromString($path, $this->stripWhitespaces(file_get_contents($file->getRealPath())));
             }
         }
 
         // Add binary file
-        $content = php_strip_whitespace($this->basePath . '/bin/gdpr-dump');
+        $content = $this->stripWhitespaces(file_get_contents($this->basePath . '/bin/gdpr-dump'));
         $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
         $phar->addFromString('bin/gdpr-dump', $content);
+    }
+
+    /**
+     * Strip whitespaces from a PHP source.
+     * This function is used instead of php_strip_whitespace, because php_strip_whitespace removes PHP annotations
+     * if the version of the PHP runtime is < 8.
+     *
+     * TODO: use php_strip_whitespace again when the min PHP version of gdpr-dump becomes 8.0.
+     *
+     * @param string $source
+     * @return string
+     */
+    private function stripWhitespaces(string $source): string
+    {
+        $output = '';
+        $isWhitespace = false;
+
+        foreach (token_get_all($source) as $token) {
+            if (is_string($token)) {
+                $output .= $token;
+                $isWhitespace = false;
+            } elseif (in_array($token[0], array(T_COMMENT, T_DOC_COMMENT))) {
+                // Remove all comments except PHP annotations
+                $output .= substr($token[1], 0, 2) === '#[' ? $token[1] : '';
+                $isWhitespace = true;
+            } elseif ($token[0] === T_WHITESPACE) {
+                // Replace everything with a single space (if previous char isn't already a space)
+                $output .= !$isWhitespace ? ' ' : '';
+                $isWhitespace = true;
+            } else {
+                $output .= $token[1];
+                $isWhitespace = false;
+            }
+        }
+
+        return $output;
     }
 
     /**
