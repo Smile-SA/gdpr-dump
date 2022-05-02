13e9660106b970763ba52cdc54a20303403e9daa
guvra <guvra@smile.fr>
2022-05-02 11:40:44 +0200

    Improve docker environment

--- docker/php-cli/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Configure composer
-[ ! -z "$COMPOSER_GITHUB_TOKEN" ] && echo $COMPOSER_GITHUB_TOKEN && composer config --global github-oauth.github.com $COMPOSER_GITHUB_TOKEN
-
-exec "$@"
