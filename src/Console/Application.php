ea038b09334b3e6e8490b43a501179141f7246fd
guvra <guvra@smile.fr>
2024-02-07 17:15:12 +0100

    Fetch application version from vendor/composer/installed.php

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,14 +4,36 @@ declare(strict_types=1);
 
 namespace Smile\GdprDump\Console;
 
+use Composer\InstalledVersions;
 use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-    public const VERSION = 'dev';
+    private const PACKAGE_NAME = 'smile/gdpr-dump';
 
     public function __construct()
     {
-        parent::__construct('GdprDump', self::VERSION);
+        parent::__construct('GdprDump', $this->getPackageVersion());
+    }
+
+    /**
+     * Get the application version.
+     */
+    private function getPackageVersion(): string
+    {
+        $prettyVersion = (string) InstalledVersions::getPrettyVersion(self::PACKAGE_NAME);
+        $reference = (string) InstalledVersions::getReference(self::PACKAGE_NAME);
+
+        if ($prettyVersion === '' || $reference === '') {
+            return 'Unknown version';
+        }
+
+        if (preg_match('/[^v\d.]/', $prettyVersion) === 0) {
+            // Tags
+            return $prettyVersion;
+        }
+
+        // Branches (with ref)
+        return $prettyVersion . '@' . substr($reference, 0, 7);
     }
 }
