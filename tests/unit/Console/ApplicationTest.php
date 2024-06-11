175b583cab45ab75763e2b81900ef6fd77465343
guvra <guvra@smile.fr>
2024-06-11 16:47:21 +0200

    Add unit test of the application class

---
+++ tests/unit/Console/ApplicationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smile\GdprDump\Tests\Unit\Console;
+
+use Smile\GdprDump\Console\Application;
+use Smile\GdprDump\Tests\Unit\TestCase;
+
+class ApplicationTest extends TestCase
+{
+    /**
+     * Test the "getVersion" method.
+     */
+    public function testVersion(): void
+    {
+        $application = new Application();
+        $installed = require __DIR__ . '/../../../vendor/composer/installed.php';
+        $version = (string) $installed['versions']['smile/gdpr-dump']['pretty_version'];
+        $reference = (string) $installed['versions']['smile/gdpr-dump']['reference'];
+
+        // In the context of a unit test, the reference is always defined
+        $expectedVersion = $version . '@' . substr($reference, 0, 7);
+        $this->assertSame($application->getVersion(), $expectedVersion);
+    }
+}
