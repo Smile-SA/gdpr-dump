6a8295f6caf4271c68587d77f6e23fc7a95bb74f
guvra <guvra@smile.fr>
2024-06-13 11:17:33 +0200

    Add unit test of the converter alias resolver

---
+++ tests/unit/DependencyInjection/ConverterAliasResolverTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smile\GdprDump\Tests\Unit\DependencyInjection;
+
+use Smile\GdprDump\DependencyInjection\Compiler\ConverterAliasPass;
+use Smile\GdprDump\DependencyInjection\ConverterAliasResolver;
+use Smile\GdprDump\Tests\Unit\TestCase;
+
+class ConverterAliasResolverTest extends TestCase
+{
+    /**
+     * Test the alias resolver.
+     */
+    public function testResolver(): void
+    {
+        $resolver = new ConverterAliasResolver();
+        $converterName = 'randomizeText';
+        $this->assertSame(ConverterAliasPass::ALIAS_PREFIX . $converterName, $resolver->getAliasByName($converterName));
+    }
+}
