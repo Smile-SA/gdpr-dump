f1f56e6e7971dff0c617bf6e5444f6c9a2d04c30
guvra <guvra@smile.fr>
2025-04-03 18:34:33 +0200

    Add database url validation

--- src/Config/Compiler/Processor/DatabaseUrlProcessor.php
+++ src/Config/Compiler/Processor/DatabaseUrlProcessor.php
@@ -4,46 +4,84 @@ declare(strict_types=1);
 
 namespace Smile\GdprDump\Config\Compiler\Processor;
 
-use Smile\GdprDump\Config\Compiler\CompileException;
+use Smile\GdprDump\Config\ConfigException;
 use Smile\GdprDump\Config\ConfigInterface;
+use Smile\GdprDump\Database\DatabaseInterface;
 
 class DatabaseUrlProcessor implements ProcessorInterface
 {
     /**
-     * Replace environment variable placeholders (e.g. "%env(DB_HOST)%")
+     * Parse database url (if specified).
      */
     public function process(ConfigInterface $config): void
     {
         $data = $config->toArray();
-        $data['database'] = $this->processDatabaseNode($config->toArray()['database'] ?? []);
+        $data['database'] = $this->processDatabaseNode($data['database']);
         $config->reset($data);
     }
 
     /**
-     * Process a config item.
+     * Parse the database url (if specified).
      *
-     * @throws CompileException
+     * @throws ConfigException
      */
-    private function processDatabaseNode(array $data): array
+    private function processDatabaseNode(array $database): array
     {
-        if (isset($data['url'])) {
-            $parsed = parse_url($data['url']);
-            $mapped = [
-                'name' => ltrim($parsed['path'] ?? '', '/'),
-                'user' => $parsed['user'] ?? null,
-                'password' => $parsed['pass'] ?? null,
-                'host' => $parsed['host'] ?? null,
-                'port' => $parsed['port'] ?? null,
-                'driver' => !empty($parsed['scheme']) ? 'pdo_' . ltrim($parsed['scheme'], 'pdo_') : null,
-            ];
+        $url = (string) ($database['url'] ?? '');
+        unset($database['url']);
 
-            foreach ($mapped as $key => $value) {
-                if (!isset($data[$key]) && (!empty($mapped[$key]))) {
-                    $data[$key] = $value;
-                }
-            }
+        if ($url === '') {
+            return $database;
         }
 
-        return $data;
+        // Validate url
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new ConfigException(sprintf('The value "%s" is not a valid URL.', $url));
+        }
+
+        // Parse url
+        $parsedUrl = parse_url($url);
+        if ($parsedUrl === false) {
+            throw new ConfigException(sprintf('Failed to parse the url "%s".', $url));
+        }
+
+        // Update database params from parsed url
+        $map = [
+            'scheme' => 'driver',
+            'path' => 'name',
+            'host' => 'host',
+            'port' => 'port',
+            'user' => 'user',
+            'pass' => 'password',
+        ];
+
+        foreach ($map as $urlPart => $dbParam) {
+            if (!array_key_exists($urlPart, $parsedUrl) || array_key_exists($dbParam, $database)) {
+                // Parameter was not found in url or is already defined in the database array
+                continue;
+            }
+
+            $value = (string) $parsedUrl[$urlPart];
+            $database[$dbParam] = match ($dbParam) {
+                'name' => ltrim($value, '/'),
+                'driver' => $this->getDriverByScheme($value),
+                default => $value
+            };
+        }
+
+        return $database;
+    }
+
+    /**
+     * Get driver by scheme.
+     *
+     * @throws ConfigException
+     */
+    private function getDriverByScheme(string $scheme): string
+    {
+        return match ($scheme) {
+            'mysql' => DatabaseInterface::DRIVER_MYSQL,
+            default => throw new ConfigException(sprintf('Invalid scheme "%s".', $scheme))
+        };
     }
 }
--- tests/unit/Config/Compiler/Processor/DatabaseUrlProcessorTest.php
+++ tests/unit/Config/Compiler/Processor/DatabaseUrlProcessorTest.php
@@ -6,28 +6,87 @@ namespace Smile\GdprDump\Tests\Unit\Config\Compiler\Processor;
 
 use Smile\GdprDump\Config\Compiler\Processor\DatabaseUrlProcessor;
 use Smile\GdprDump\Config\Config;
+use Smile\GdprDump\Config\ConfigException;
 use Smile\GdprDump\Tests\Unit\TestCase;
 
 class DatabaseUrlProcessorTest extends TestCase
 {
     /**
-     * Assert that database URL is processed and parts (i.e. password) take precedence
+     * Assert that the url is processed and individual params (e.g. password) take precedence.
      */
-    public function testEnvVarProcessor(): void
+    public function testDatabaseUrlProcessor(): void
     {
         $data = [
-            'database' =>
-                [
-                    'password' => 'another_secret_password',
-                    'url' => 'mysql://foo:secret_password@localhost/databasename',
-                ],
+            'database' => [
+                'password' => 'another_secret_password',
+                'url' => 'mysql://foo:secret_password@localhost/database_name',
+            ],
         ];
 
         $config = new Config($data);
         $processor = new DatabaseUrlProcessor();
         $processor->process($config);
 
-        $this->assertSame('localhost', $config->get('database')['host']);
-        $this->assertSame('another_secret_password', $config->get('database')['password']);
+        $dbParams = $config->get('database');
+        $this->assertSame('database_name', $dbParams['name']);
+        $this->assertSame('localhost', $dbParams['host']);
+        $this->assertSame('foo', $dbParams['user']);
+        $this->assertSame('another_secret_password', $dbParams['password']);
+        $this->assertArrayNotHasKey('port', $dbParams);
+    }
+
+    /**
+     * Assert that the url is processed properly when only the scheme and path are defined.
+     */
+    public function testUrlWithoutDatabaseName(): void
+    {
+        $data = [
+            'database' => [
+                'url' => 'mysql://localhost',
+            ],
+        ];
+
+        $config = new Config($data);
+        $processor = new DatabaseUrlProcessor();
+        $processor->process($config);
+
+        $dbParams = $config->get('database');
+        $this->assertSame('localhost', $dbParams['host']);
+    }
+
+    /**
+     * Assert that an exception is thrown when an invalid url is specified.
+     */
+    public function testInvalidUrl(): void
+    {
+        $data = [
+            'database' => [
+                'url' => 'invalid',
+            ],
+        ];
+
+        $config = new Config($data);
+        $processor = new DatabaseUrlProcessor();
+
+        $this->expectException(ConfigException::class);
+        $processor->process($config);
+    }
+
+    /**
+     * Assert that an exception is thrown when an invalid driver is specified.
+     */
+    public function testInvalidDriver(): void
+    {
+        $data = [
+            'database' => [
+                'url' => 'invalid://foo:secret_password@localhost/database_name',
+            ],
+        ];
+
+        $config = new Config($data);
+        $processor = new DatabaseUrlProcessor();
+
+        $this->expectException(ConfigException::class);
+        $processor->process($config);
     }
 }
