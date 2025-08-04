e10b24f75962b4247887cd0f69715eca84f60726
guvra <guvra@smile.fr>
2025-08-04 18:06:14 +0200

    Add a builder for the database connection params

--- src/Database/DatabaseFactory.php
+++ src/Database/DatabaseFactory.php
@@ -4,28 +4,25 @@ declare(strict_types=1);
 
 namespace Smile\GdprDump\Database;
 
+use Doctrine\DBAL\Exception;
 use Smile\GdprDump\Config\ConfigInterface;
+use Smile\GdprDump\Database\Builder\ConnectionParamsBuilder;
 
 final class DatabaseFactory
 {
+    public function __construct(private ConnectionParamsBuilder $connectionParamsBuilder)
+    {
+    }
+
     /**
      * Create a database object.
+     *
+     * @throws Exception
      */
     public function create(ConfigInterface $config): Database
     {
-        $connectionParams = (array) $config->get('database', []);
+        $connectionParams = $this->connectionParamsBuilder->build($config);
 
-        // Rename some keys (for compatibility with the Doctrine connection)
-        if (array_key_exists('name', $connectionParams)) {
-            $connectionParams['dbname'] = $connectionParams['name'];
-            unset($connectionParams['name']);
-        }
-
-        if (array_key_exists('driver_options', $connectionParams)) {
-            $connectionParams['driverOptions'] = $connectionParams['driver_options'];
-            unset($connectionParams['driver_options']);
-        }
-
-        return new Database($connectionParams);
+        return new Database(new ParameterBag($connectionParams));
     }
 }
